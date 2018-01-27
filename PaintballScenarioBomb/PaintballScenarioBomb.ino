b7c0bd15f58622595b9581956ce541cb096167d5
Christiaan <christiaan@iMac.local>
2018-01-27 18:27:01 +0200

    Created .ino Arduino Sketch
    Empty sketch to enable online edit.

---
+++ PaintballScenarioBomb/PaintballScenarioBomb.ino
@@ -0,0 +1,23 @@
+/******************************************************************************************
+Description: Paintball Scenraio Bomb by Ginganinja82
+Code Repo: http://github.com/Ginganinja82/Paintball_Scenario_Bomb
+Designs: 
+Last Commit: N/A
+*******************************************************************************************/
+#include <Tone.h>
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
+
+
+Tone tone1;
+#define 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
